48fc3477409b0d590d8d083032fb45254ada0338
Denitsa Dencheva <denitza.d@gmail.com>
2020-10-23 15:01:13 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+                System.out.println("My First Git Project");
+                System.out.println("Hej Denitsa");
+            }
+        }
+
+
